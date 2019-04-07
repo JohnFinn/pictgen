488ab0af2075ad7a50e406f9c3a7de1560a85666
Jouni Sunnari <Jouny@tuta.io>
2019-04-07 20:52:59 +0300

    image drawing example

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 function main() {
     var canvas = document.createElement('canvas');
     document.body.appendChild(canvas);
+    var canvasContext = canvas.getContext('2d');
+    var image = new Image();
+    image.src = '1.jpg';
+    image.onload = function(){
+        console.log('image loaded');
+        canvasContext.drawImage(image, 0, 0, 100, 100);
+    };
 }
 
 window.onload = main;
